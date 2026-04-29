098e347b50c11a048fc8d574e166047cc541a9b4
Chris Jones <chris@cjmobileapps.com>
2026-04-28 23:49:33 -0500

    Add rest of houses

--- src/data/mock_data.rs
+++ src/data/mock_data.rs
@@ -1,25 +1,35 @@
-use std::sync::LazyLock;
 use crate::data::model::house::{House, HouseName};
+use std::sync::LazyLock;
 
 pub struct MockData;
 
 impl MockData {
-    
     pub(crate) fn get_houses() -> &'static Vec<House> {
-       &HOUSES
+        &HOUSES
     }
-
-   
 }
 
 static HOUSES: LazyLock<Vec<House>> = LazyLock::new(|| {
-    let gryffindor_house = House {
-        name: HouseName::Gryffindor,
-        image_url: "https://static.wikia.nocookie.net/harrypotter/images/9/98/Gryffindor.jpg/revision/latest".to_string(),
-        emoji: "\u{1F981}".to_string(),
-    };
-    
-    vec![gryffindor_house]
+    vec![
+        House {
+            name: HouseName::Gryffindor,
+            image_url: "https://static.wikia.nocookie.net/harrypotter/images/9/98/Gryffindor.jpg/revision/latest".to_string(),
+            emoji: "\u{1F981}".to_string(),
+        },
+        House {
+            name: HouseName::Slytherin,
+            image_url: "https://static.wikia.nocookie.net/harrypotter/images/6/6e/Slytherin.jpg/revision/latest".to_string(),
+            emoji: "\u{1F40D}".to_string(),
+        },
+        House {
+            name: HouseName::Ravenclaw,
+            image_url: "https://static.wikia.nocookie.net/harrypotter/images/3/3c/RavenclawCrest.jpg/revision/latest".to_string(),
+            emoji: "\u{1F985}".to_string(),
+        },
+        House {
+            name: HouseName::Hufflepuff,
+            image_url: "https://static.wikia.nocookie.net/harrypotter/images/e/e4/Hufflepuff.jpg/revision/latest".to_string(),
+            emoji: "\u{1F9A1}".to_string(),
+        },
+    ]
 });
-
-
